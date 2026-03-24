af6195058baef10b5b3ff4acaaef1208c8e1f156
Marc Pont <mpont91@gmail.com>
2026-03-24 12:09:05 +0100

    Adding scenes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import typescriptParser from '@typescript-eslint/parser'
 
 export default [
   {
     ignores: ['.astro/**', 'dist/**', 'node_modules/**'],
   },
   ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: typescriptParser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
   eslintConfigPrettier,
 ]
